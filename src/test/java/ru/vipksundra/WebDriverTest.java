4cb7462f98fc0af9d4ded74ed4cbaec0f5a77e88
KMitrofanova <vip.ksundra@mail.ru>
2024-01-21 00:29:22 +0300

    updated WebDriverTest

--- src/test/java/ru/vipksundra/WebDriverTest.java
+++ src/test/java/ru/vipksundra/WebDriverTest.java
@@ -1,4 +1,17 @@
 package ru.vipksundra;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WebDriverTest {
+    @Test
+    public void testGithub(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://github.com");
+        String title = driver.getTitle();
+        assertEquals (title,"");
+    }
 }
